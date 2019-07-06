b5972272f2e5fdac8d5d5725687f58f7dc3c2892
YagoYJ <yagojuanyjskt@gmail.com>
2019-07-06 11:20:20 -0300

    Precisa editar as fotos

---
+++ brownies.php
@@ -0,0 +1,106 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <link rel='stylesheet' href='https://use.fontawesome.com/releases/v5.7.0/css/all.css'>
+        <link rel="stylesheet" href="css/padrao.css">
+        <link rel="stylesheet" href="css/brownies.css">
+        <title>Brownies</title>
+    </head>
+
+    <body>
+        <div id="divCabecalho">
+            <header>
+                <nav>
+                    <a href="index.php"><img src="img/Logo-header.jpg" alt="Logo"></a>
+                    <ul>
+                        <li id="liInicio"><a href="index.php">Início</a></li>
+                        <li id="liBrownies"><a href="brownies.php">Brownies</a></li>
+                        <li id="liQuemSomos"><a href="quemSomos.php">Quem somos</a></li>
+                    </ul>
+                </nav>
+            </header>
+        </div>
+
+        <div id="divCorpo">
+            <h1>OS MELHORES BROWNIES DE FORTALEZA!</h1>
+
+            <div class="produto" id="prod1">
+                <a>
+                    <h2>foto 1</h2>
+                    <p>Nome do produto <span>R$ 5,00</span></p>
+                </a>
+            </div>
+            <div class="produto" id="prod1">
+                <h2>foto 2</h2>
+                <p>Nome do produto <span>R$ 5,00</span></p>
+            </div>
+            <div class="produto" id="prod1">
+                <h2>foto 3</h2>
+                <p>Nome do produto <span>R$ 5,00</span></p>
+            </div>
+            <div class="produto" id="prod1">
+                <h2>foto 4</h2>
+                <p>Nome do produto <span>R$ 5,00</span></p>
+            </div>
+            <div class="produto" id="prod1">
+                <h2>foto 5</h2>
+                <p>Nome do produto <span>R$ 5,00</span></p>
+            </div>
+            <div class="produto" id="prod1">
+                <h2>foto 6</h2>
+                <p>Nome do produto <span>R$ 5,00</span></p>
+            </div>
+            <div class="produto" id="prod1">
+                <h2>foto 7</h2>
+                <p>Nome do produto <span>R$ 5,00</span></p>
+            </div>
+            <div class="produto" id="prod1">
+                <h2>foto 8</h2>
+                <p>Nome do produto <span>R$ 5,00</span></p>
+            </div>
+            <div class="produto" id="prod1">
+                <h2>foto 9</h2>
+                <p>Nome do produto <span>R$ 5,00</span></p>
+            </div>
+            <div class="produto" id="prod1">
+                <h2>foto 10</h2>
+                <p>Nome do produto <span>R$ 5,00</span></p>
+            </div>
+            <div class="produto" id="prod1">
+                <h2>foto 11</h2>
+                <p>Nome do produto <span>R$ 5,00</span></p>
+            </div>
+            <div class="produto" id="prod1">
+                <h2>foto 12</h2>
+                <p>Nome do produto <span>R$ 5,00</span></p>
+            </div>
+            <div class="produto" id="prod1">
+                <h2>foto 13</h2>
+                <p>Nome do produto <span>R$ 5,00</span></p>
+            </div>
+            <div class="produto" id="prod1">
+                <h2>foto 14</h2>
+                <p>Nome do produto <span>R$ 5,00</span></p>
+            </div>
+            <div class="produto" id="prod1">
+                <h2>foto 15</h2>
+                <p>Nome do produto <span>R$ 5,00</span></p>
+            </div>
+
+        </div>
+
+        <div id="divRodape">
+            <footer>
+                <i class="fab fa-facebook"></i>
+                <i class="fab fa-instagram"></i>
+                <p>&copy; Todos os direitos reservados - Super Brownie</p>
+            </footer>
+        </div>
+
+    </body>
+
+</html>
---
+++ index.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <link rel='stylesheet' href='https://use.fontawesome.com/releases/v5.7.0/css/all.css'>
+        <link rel="stylesheet" href="css/padrao.css">
+        <link rel="stylesheet" href="css/index.css">
+        <title>Página inicial</title>
+    </head>
+
+    <body>
+        <div id="divCabecalho">
+            <header>
+                <nav>
+                    <a href="index.php"><img src="img/Logo-header.jpg" alt="Logo"></a>
+                    <ul>
+                        <li id="liInicio"><a href="index.php">Início</a></li>
+                        <li id="liBrownies"><a href="brownies.php">Brownies</a></li>
+                        <li id="liQuemSomos"><a href="quemSomos.php">Quem somos</a></li>
+                    </ul>
+                </nav>
+            </header>
+        </div>
+
+        <div id="divCorpo">
+            <h2>MAIS VENDIDOS</h2>
+
+            <div id="divFotosMaisVendidos">
+                <a><img src="img/prod1.jpeg" alt="Produto dos mais vendidos"></a>
+                <a><img src="img/prod2.jpeg" alt="Produto dos mais vendidos"></a>
+                <a><img src="img/prod3.jpeg" alt="Produto dos mais vendidos"></a>
+                <a href="brownies.php"><button id="btnVerMais">Ver mais</button></a>
+            </div>
+
+
+        </div>
+
+        <div id="divRodape">
+            <footer>
+                <i class="fab fa-facebook"></i>
+                <i class="fab fa-instagram"></i>
+                <p>&copy; Todos os direitos reservados - Super Brownie</p>
+            </footer>
+        </div>
+
+    </body>
+
+</html>
